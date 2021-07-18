c0d1ec2acc0e0c2a87bf9a69b84dda7b9f25881a
umer ali <sheikhumer973@gmail.com>
2021-07-17 17:11:45 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AsidemenuComponent } from './components/asidemenu/asidemenu.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    SidemenuComponent,
+    FooterComponent,
+    AsidemenuComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
